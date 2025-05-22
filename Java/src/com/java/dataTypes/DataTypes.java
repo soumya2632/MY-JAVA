95734616e67e7191f15c36cee3cc006499765bc2
pc <pc@DESKTOP-05OCEHO>
2025-05-22 04:04:09 -0700

    data types

--- Java/src/com/java/dataTypes/DataTypes.java
+++ Java/src/com/java/dataTypes/DataTypes.java
@@ -1,5 +1,24 @@
 package com.java.dataTypes;
 
 public class DataTypes {
-
-}
+	 public static void main(String args[])  
+	    {  
+	        boolean flag = true;  
+	        char ch = 'z';  
+	        int num = 1234;  
+	        byte size = 2;  
+	        short srt = 78;  
+	        double value = 2.4546778;  
+	        float temp = 3.8f;  
+	        long val = 1888889;  
+	        System.out.println("boolean: " + flag);  
+	        System.out.println("char: " + ch);  
+	        System.out.println("integer: " + num);  
+	        System.out.println("byte: " + size);  
+	        System.out.println("short: " + srt);  
+	        System.out.println("float: " + value);  
+	        System.out.println("double: " + temp);  
+	        System.out.println("long: " + val);  
+	    }  
+	}  
+	
